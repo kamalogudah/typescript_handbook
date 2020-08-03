7d89fd226396fcf92fa7bdb2972479af60c14e80
kamalogudah <mcpaul2058@gmail.com>
2020-08-03 13:33:58 +0300

    Overload

--- functions.ts
+++ functions.ts
@@ -143,3 +143,58 @@ let cardPicker2 = deck.createCardPicker();
 let pickedCard2 = cardPicker();
 
 alert('card: ' + pickedCard.card + ' of ' + pickedCard.suit);
+
+//this parameters in callbacks #
+interface UIElement {
+    addClickListener(onclick: (this: void, e: Event) => void): void;
+}
+
+class Handler {
+    info: string;
+    onClickBad(this: Handler, e: Event) {
+        // oops, used `this` here. using this callback would crash at runtime
+        this.info = e.message;
+    }
+}
+let h = new Handler();
+uiElement.addClickListener(h.onClickBad); // error!
+
+class Handler {
+  info: string;
+  onClickGood(this: void, e: Event) {
+    // can't use `this` here because it's of type void!
+    console.log('clicked!');
+  }
+}
+let h = new Handler();
+uiElement.addClickListener(h.onClickGood);//no error
+
+// Overloads #
+let suits = ['hearts', 'spades', 'clubs', 'diamonds'];
+
+function pickCard(x: { suit: string; card: number }[]): number;
+function pickCard(x: number): { suit: string; card: number };
+function pickCard(x): any {
+  // Check to see if we're working with an object/array
+  // if so, they gave us the deck and we'll pick the card
+  if (typeof x == 'object') {
+    let pickedCard = Math.floor(Math.random() * x.length);
+    return pickedCard;
+  }
+  // Otherwise just let them pick the card
+  else if (typeof x == 'number') {
+    let pickedSuit = Math.floor(x / 13);
+    return { suit: suits[pickedSuit], card: x % 13 };
+  }
+}
+
+let myDeck = [
+  { suit: 'diamonds', card: 2 },
+  { suit: 'spades', card: 10 },
+  { suit: 'hearts', card: 4 },
+];
+let pickedCard1 = myDeck[pickCard(myDeck)];
+alert('card: ' + pickedCard1.card + ' of ' + pickedCard1.suit);
+
+let pickedCard2 = pickCard(15);
+alert('card: ' + pickedCard2.card + ' of ' + pickedCard2.suit);
