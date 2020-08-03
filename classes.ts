787017de3ebcd1b89b9fa0023bdb6a5d48a6aaa3
kamalogudah <mcpaul2058@gmail.com>
2020-08-03 10:03:58 +0300

    Public methods

--- classes.ts
+++ classes.ts
@@ -63,3 +63,22 @@ let tom: Animal = new Horse('Tommy the Palomino');
 
 sam.move();
 tom.move(34);
+
+// public by default 
+class Animal {
+  public name: string;
+  public constructor(theName: string) {
+    this.name = theName;
+  }
+  public move(distanceInMeters: number) {
+    console.log(`${this.name} moved ${distanceInMeters}m.`);
+  }
+}
+
+// ecmascript private fields
+class Animal {
+    #name: string;
+    constructor(theName: string) { this.#name = theName; }
+}
+
+new Animal("Cat").#name; // Property '#name' is not accessible outside class 'Animal' because it has a private identifier.
