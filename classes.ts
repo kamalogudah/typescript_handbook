769ac59907962c4a7d8d513a50dc732ea005b90a
kamalogudah <mcpaul2058@gmail.com>
2020-08-03 11:09:12 +0300

    Constructor Functions

--- classes.ts
+++ classes.ts
@@ -241,4 +241,28 @@ department = new AccountingDepartment(); // ok to create and assign a non-abstra
 department.printName();
 department.printMeeting();
 department.generateReports(); // error: method doesn't exist on declared abstract type
-Advanced Techniques #
+
+// Advanced Techniques #constructor functions
+
+class Greeter {
+    static standardGreeting = "Hello, there";
+    greeting: string;
+    greet() {
+        if (this.greeting) {
+            return "Hello, " + this.greeting;
+        }
+        else {
+            return Greeter.standardGreeting;
+        }
+    }
+}
+
+let greeter1: Greeter;
+greeter1 = new Greeter();
+console.log(greeter1.greet()); // "Hello, there"
+
+let greeterMaker: typeof Greeter = Greeter;
+greeterMaker.standardGreeting = "Hey there!";
+
+let greeter2: Greeter = new greeterMaker();
+console.log(greeter2.greet()); // "Hey there!"
