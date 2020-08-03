a662c0564c237354e0a396941f779c2fc1e375e7
kamalogudah <mcpaul2058@gmail.com>
2020-08-03 13:15:32 +0300

    Rest parameters

--- functions.ts
+++ functions.ts
@@ -67,3 +67,11 @@ let result1 = buildName("Bob");                  // error, too few parameters
 let result2 = buildName("Bob", "Adams", "Sr.");  // error, too many parameters
 let result3 = buildName("Bob", "Adams");         // okay and returns "Bob Adams"
 let result4 = buildName(undefined, "Adams");     // okay and returns "Will Adams"
+
+// Rest parameters
+function buildName(firstName: string, ...restOfName: string[]) {
+  return firstName + ' ' + restOfName.join(' ');
+}
+
+// employeeName will be "Joseph Samuel Lucas MacKinzie"
+let employeeName = buildName('Joseph', 'Samuel', 'Lucas', 'MacKinzie');
